c17af5810003bb9b5f42921c0f3ff2dd8aa96be0
hendi.dp <hendi.dp@pttmg.com>
2026-05-09 12:48:18 +0700

    implemented SimpleMapper object

---
+++ HendiMapper/HendiMapper/Core/SimpleMapper.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+
+namespace HendiMapper.Core;
+
+public static class SimpleMapper
+{
+    public static TDestination Map<TDestination>(object source)
+        where TDestination : new()
+    {
+        if (source == null)
+            return default!;
+
+        var destination = new TDestination();
+
+        Merge(destination, source);
+
+        return destination;
+    }
+
+    public static TDestination Merge<TDestination, TSource>(
+        TDestination destination,
+        TSource source)
+    {
+        if (destination == null || source == null)
+            return destination;
+
+        // FIX BUG HERE
+        var sourceProperties = source.GetType()
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        var destinationProperties = typeof(TDestination)
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var sourceProp in sourceProperties)
+        {
+            var destProp = destinationProperties
+                .FirstOrDefault(x =>
+                    x.Name == sourceProp.Name &&
+                    x.PropertyType == sourceProp.PropertyType &&
+                    x.CanWrite);
+
+            if (destProp == null)
+                continue;
+
+            var value = sourceProp.GetValue(source);
+
+            if (value == null)
+                continue;
+
+            destProp.SetValue(destination, value);
+        }
+
+        return destination;
+    }
+}
